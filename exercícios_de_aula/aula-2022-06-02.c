67a6989836ac032df396e69bc823714e33aef96d
victorsaluci <106778918+victorsaluci@users.noreply.github.com>
2022-06-02 19:49:18 -0300

    Update aula-2022-06-02.c

--- exercícios_de_aula/aula-2022-06-02.c
+++ exercícios_de_aula/aula-2022-06-02.c
@@ -1,1 +1,35 @@
+/*
+ler as variáveis reais para cada dia com scanf
+
+/*
+#include <stdio.h>
+int main ()
+{
+float segunda, terca, quarta, quinta,sexta, soma, paga;
+printf ("informe o valor recebido na segunda:\n");
+scanf ("%f", & segunda);
+printf ("informe o valor da despesa de segunda: \n");
+scanf ("%f", &pagaseg);
+printf ("informe o valor recebido na terca:\n");
+scanf ("%f", & terca);
+printf ("informe o valor recebido na terca:\n");
+scanf ("%f", &pagater);
+printf ("informe o valor recebido na quarta:\n");
+scanf ("%f", & quarta);
+printf ("informe o valor recebido na quarta:\n");
+scanf ("%f", &pagaqua);
+printf ("informe o valor recebido na quinta:\n");
+scanf ("%f", & quinta);
+printf ("informe o valor recebido na quinta:\n");
+scanf ("%f", &pagaquin);
+printf ("informe o valor recebido na sexta:\n");
+scanf ("%f", & sexta);
+printf ("informe o valor recebido na sexta:\n");
+scanf ("%f", &pagasex);
+soma= segunda+terca+quarta+quinta+sexta;
+saldo= pagaseg+pagater+pagaqua+pagaquin+pagasex
+saldopos= soma-saldo
+if soma > 2000
+printf("o valor recebido na semana é:
+
 
