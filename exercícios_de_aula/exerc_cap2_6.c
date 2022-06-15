05c1aa7ae5556eb53ead035920cafe6574080f8c
victorsaluci <106778918+victorsaluci@users.noreply.github.com>
2022-06-15 20:21:37 -0300

    Create exerc_cap2_6.c

---
+++ exercícios_de_aula/exerc_cap2_6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+ float salario, prestacao, credito;
+ prestacao = salario*0,3;
+ printf ("Digite seu salário: \n");
+ scanf ("%f", & salario);
+ printf ("Digite o valor desejado da prestação");
+ scanf ("%f", & prestacao);
+ if (crédito <= prestacao)
+   printf ("O empréstimo pode ser concedido! \n");
+ else 
+    printf ("O empréstimo não pode ser concedido \n");
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
